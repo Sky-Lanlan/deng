eb58cc6bf464ea66bb9f1ad41faae2e6addfff04
nanLan <2235826534@qq.com>
2018-12-23 22:16:10 +0800

    init

---
+++ train_ticket/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='tickets',
+    version='0.0.1',
+    py_modules=['tickets', 'stations'],
+    install_requires=['requests', 'docopt', 'prettytable', 'colorama'],
+    entry_points={
+        'console_scripts': ['tickets=tickets:cli']
+    }
+)
